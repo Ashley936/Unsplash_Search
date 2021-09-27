bdce0c0981180b5c23ff62b88481f24ea0166265
Ashley <namitarastogimwn@gmail.com>
2021-09-28 03:40:32 +0530

    Secure api key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID ttw5nAsRdtu1J85otf_9G9YmpjeY9jSY-u0WlSswfgk",
-  }
+    Authorization: `Client-ID ${process.env.REACT_APP_GOOGLE_API_KEY}`,
+  },
 });
